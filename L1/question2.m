8e28868362ff263ee0f71005d282558e4bade9a8
K-rolls <krolls@mun.ca>
2025-06-10 13:18:31 -0230

    Update formatting

--- L1/question2.m
+++ L1/question2.m
@@ -1,21 +1,21 @@
-imc = imread('im2.png');% Read the image
+imc = imread('im2.png'); % Read the image
 img = rgb2gray(imc); % Convert to grayscale
 imshow(img); % View image
 imwrite(img, 'L1/report/img2_gray.png'); % Save the grayscale image
 
-H = size(img,1); % Read the height of the image
-W = size(img,2); % Read the width of the image
+H = size(img, 1); % Read the height of the image
+W = size(img, 2); % Read the width of the image
 
-Hist_arr = zeros(1,256); % Array for holding the (original) histogram
-Hist_eq_arr = zeros(1,256); % Array for holding the (equalized) histogram
+Hist_arr = zeros(1, 256); % Array for holding the (original) histogram
+Hist_eq_arr = zeros(1, 256); % Array for holding the (equalized) histogram
 
-CDF_array = zeros(1,length(Hist_arr)); % array to hold cumulative distribution function (CDF)
+CDF_array = zeros(1, length(Hist_arr)); % array to hold cumulative distribution function (CDF)
 
-hist_eq_img = uint8(zeros(H,W)); % A 2D array for keeping histogram equalized image (intensity in 8 bit integers)
+hist_eq_img = uint8(zeros(H, W)); % A 2D array for keeping histogram equalized image (intensity in 8 bit integers)
 
-for i=1:H,
-    for j=1:W,
-        Hist_arr(1,img(i,j)+1)=Hist_arr (1,img(i,j)+1)+1 ;
+for i = 1:H
+    for j = 1:W
+        Hist_arr(1, img(i, j) + 1) = Hist_arr (1, img(i, j) + 1) + 1;
     end
 end
 
@@ -24,21 +24,22 @@ bar(Hist_arr);
 title('Histogram of Original Image');
 saveas(gcf, 'L1/report/img2_hist_orig.png', 'png');
 
-Hist_arr_pdf = Hist_arr/(H*W); % PDF
+Hist_arr_pdf = Hist_arr / (H * W); % PDF
 
 figure;
 plot(0:255, Hist_arr_pdf, 'r-');
 xlabel('Gray Level');
 ylabel('Probability Density Function (PDF)');
 grid on;
-axis([0 255 0 max(Hist_arr_pdf)*1.1]);
+axis([0 255 0 max(Hist_arr_pdf) * 1.1]);
 title('PDF of Original Image');
 saveas(gcf, 'L1/report/img2_pdf_orig.png', 'png');
 
-dummy1=0; % A dummy variable to hold the summation results
-for k=1:length(Hist_arr); % Generating the CDF from PDF
-    dummy1=dummy1+Hist_arr_pdf(k);
-    CDF_array(k)= dummy1;
+dummy1 = 0; % A dummy variable to hold the summation results
+
+for k = 1:length(Hist_arr) % Generating the CDF from PDF
+    dummy1 = dummy1 + Hist_arr_pdf(k);
+    CDF_array(k) = dummy1;
 end
 
 figure;
@@ -49,27 +50,30 @@ ylabel('Cumulative Probability');
 grid on;
 saveas(gcf, 'L1/report/img2_cdf_orig.png', 'png');
 
-for l=1:H, % Histogram equalization
-    for m=1:W,
-        hist_eq_img(l,m)= round(CDF_array(img(l,m)+1)*(length(Hist_arr_pdf)-1)); % scale to 255 and round to nearest integer
-        Hist_eq_arr(1, hist_eq_img(l,m)+1)=Hist_eq_arr (1,hist_eq_img(l,m)+1)+1; % Its histogram
+for l = 1:H % Histogram equalization
+
+    for m = 1:W
+        hist_eq_img(l, m) = round(CDF_array(img(l, m) + 1) * (length(Hist_arr_pdf) - 1)); % scale to 255 and round to nearest integer
+        Hist_eq_arr(1, hist_eq_img(l, m) + 1) = Hist_eq_arr (1, hist_eq_img(l, m) + 1) + 1; % Its histogram
     end
+
 end
 
 % Display the histogram of equalized image
 figure;
-subplot(2,1,1);
+subplot(2, 1, 1);
 bar(Hist_eq_arr);
 title('Histogram of Equalized Image');
 
 % Create a mapping table showing original gray levels and their new values
 mapping = zeros(1, 256);
+
 for i = 1:256
     mapping(i) = round(CDF_array(i) * 255);
 end
 
 % Display the mapping
-subplot(2,1,2);
+subplot(2, 1, 2);
 plot(0:255, mapping, 'r-', 0:255, 0:255, 'b--');
 title('Mapping from Original to Equalized Gray Levels');
 xlabel('Original Gray Level');
--- L1/question3.m
+++ L1/question3.m
@@ -5,21 +5,21 @@ imc = imread("./L1/im3.png");
 img = rgb2gray(imc);
 
 % Rotate the image by 90 degrees
-theta = pi/2;
+theta = pi / 2;
 
 % Transformation matrix for rotation
 R = [cos(theta) sin(theta) 0;
-    -sin(theta) cos(theta) 0;
-    0           0          1];
+     -sin(theta) cos(theta) 0;
+     0 0 1];
 
 function [rot_img] = rotate_image(img, R)
     % Rotate image `img` using the transformation matrix `R`
     [y_max, x_max] = size(img);
     fprintf('Image size: %d x %d\n', y_max, x_max);
     corners = [0, 0, 1;
-        x_max, 0, 1;
-        0, y_max, 1;
-        x_max, y_max, 1]; % Corner points of the image
+               x_max, 0, 1;
+               0, y_max, 1;
+               x_max, y_max, 1]; % Corner points of the image
 
     new_corners = corners * R; % New location of corners
     new_width = round(max(new_corners(:, 1)) - min(new_corners(:, 1)));
@@ -30,6 +30,7 @@ function [rot_img] = rotate_image(img, R)
     min_height = round(min_height) + 1;
 
     rot_img = zeros(new_height, new_width);
+
     for i = 1:y_max
         for j = 1:x_max
             temp = ([j - 1, i - 1, 1]) * R;
@@ -48,7 +49,7 @@ custom_time = toc;
 tic; % Start timing
 % Create the transformation matrix using maketform
 tform = maketform('affine', R);
-% Apply the transformation using imtransform      
+% Apply the transformation using imtransform
 rot_img_inbuilt = imtransform(img, tform);
 inbuilt_time = toc; % End timing
 
@@ -61,18 +62,18 @@ set(gcf, 'Units', 'normalized');
 p = get(gcf, 'Position');
 
 subplot(2, 2, 1);
-imshow(imc,[]);
+imshow(imc, []);
 title('Original Image');
 
 subplot(2, 2, 2);
 imshow(rot_img_inbuilt, []);
 title('Rotated Image (Inbuilt Function)');
-text(size(rot_img_inbuilt,2)-100, size(rot_img_inbuilt,1)-20, sprintf('Time: %.4f sec', inbuilt_time), 'Color', 'white', 'FontSize', 8, 'BackgroundColor', [0 0 0 0.5]);
+text(size(rot_img_inbuilt, 2) - 100, size(rot_img_inbuilt, 1) - 20, sprintf('Time: %.4f sec', inbuilt_time), 'Color', 'white', 'FontSize', 8, 'BackgroundColor', [0 0 0 0.5]);
 
 subplot(2, 2, 3);
 imshow(rot_img_custom, []);
 title('Rotated Image (Custom Code)');
-text(size(rot_img_custom,2)-100, size(rot_img_custom,1)-20, sprintf('Time: %.4f sec', custom_time), 'Color', 'white', 'FontSize', 8, 'BackgroundColor', [0 0 0 0.5]);
+text(size(rot_img_custom, 2) - 100, size(rot_img_custom, 1) - 20, sprintf('Time: %.4f sec', custom_time), 'Color', 'white', 'FontSize', 8, 'BackgroundColor', [0 0 0 0.5]);
 
 subplot(2, 2, 4);
 bar([custom_time, inbuilt_time]);
@@ -88,48 +89,49 @@ fprintf('Time taken using inbuilt functions: %.4f seconds\n', inbuilt_time);
 % save the figure
 saveas(gcf, './L1/report/img3_q1.png', 'png');
 
-H = size(img,1); % Read the height of the image
-W = size(img,2); % Read the width of the image
+H = size(img, 1); % Read the height of the image
+W = size(img, 2); % Read the width of the image
 
-Hist_arr = zeros(1,256); % Array for holding the (original) histogram
-Hist_eq_arr = zeros(1,256); % Array for holding the (equalized) histogram
+Hist_arr = zeros(1, 256); % Array for holding the (original) histogram
+Hist_eq_arr = zeros(1, 256); % Array for holding the (equalized) histogram
 
-CDF_array = zeros(1,length(Hist_arr)); % array to hold cumulative distribution function (CDF)
+CDF_array = zeros(1, length(Hist_arr)); % array to hold cumulative distribution function (CDF)
 
-hist_eq_img = uint8(zeros(H,W)); % A 2D array for keeping histogram equalized image (intensity in 8 bit integers)
+hist_eq_img = uint8(zeros(H, W)); % A 2D array for keeping histogram equalized image (intensity in 8 bit integers)
 
-for i=1:H,
-    for j=1:W,
-        Hist_arr(1,img(i,j)+1)=Hist_arr (1,img(i,j)+1)+1 ;
+for i = 1:H
+    for j = 1:W
+        Hist_arr(1, img(i, j) + 1) = Hist_arr (1, img(i, j) + 1) + 1;
     end
 end
 
-Hist_arr_pdf = Hist_arr/(H*W); % PDF
+Hist_arr_pdf = Hist_arr / (H * W); % PDF
 
-dummy1=0; % A dummy variable to hold the summation results
-for k=1:length(Hist_arr); % Generating the CDF from PDF
-    dummy1=dummy1+Hist_arr_pdf(k);
-    CDF_array(k)= dummy1;
+dummy1 = 0; % A dummy variable to hold the summation results
+
+for k = 1:length(Hist_arr) % Generating the CDF from PDF
+    dummy1 = dummy1 + Hist_arr_pdf(k);
+    CDF_array(k) = dummy1;
 end
 
-for l=1:H, % Histogram equalization
-    for m=1:W,
-        hist_eq_img(l,m)= round(CDF_array(img(l,m)+1)*(length(Hist_arr_pdf)-1)); % scale to 255 and round to nearest integer
-        Hist_eq_arr(1, hist_eq_img(l,m)+1)=Hist_eq_arr (1,hist_eq_img(l,m)+1)+1; % Its histogram
+for l = 1:H % Histogram equalization
+    for m = 1:W
+        hist_eq_img(l, m) = round(CDF_array(img(l, m) + 1) * (length(Hist_arr_pdf) - 1)); % scale to 255 and round to nearest integer
+        Hist_eq_arr(1, hist_eq_img(l, m) + 1) = Hist_eq_arr (1, hist_eq_img(l, m) + 1) + 1; % Its histogram
     end
 end
 
 figure;
-subplot(2,2,1);
+subplot(2, 2, 1);
 imshow(img);
 title('Original Image');
-subplot(2,2,2);
+subplot(2, 2, 2);
 bar(Hist_arr);
 title('Histogram of Original Image');
-subplot(2,2,3);
+subplot(2, 2, 3);
 imshow(hist_eq_img);
 title('Histogram Equalized Image');
-subplot(2,2,4);
+subplot(2, 2, 4);
 bar(Hist_eq_arr);
 title('Histogram of Equalized Image');
 
